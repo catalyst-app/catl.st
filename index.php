6b96a765709f217ee161acaacd55047d6861877e
Fauxil-Fox <fauxil@catalystapp.co>
2018-07-10 22:54:45 -0400

    Add 301 redirect

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ if (empty($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] == "off") {
 
 // WWWs are annoying and icky
 if (strpos($_SERVER["HTTP_HOST"], "www.") === 0) {
+	header("HTTP/1.1 301 Moved Permanently");
+	
 	header("Location: "."https://".substr($_SERVER["HTTP_HOST"], 4).$_SERVER["REQUEST_URI"]);
 
 	die("Redirecting...");
