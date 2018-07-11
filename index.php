246961e1e6ec26e475230e1e1291cdbcd00695ff
Fauxil Fox <fauxil@catalystapp.co>
2018-07-11 14:23:30 -0400

    Add stream links to URL shortener

--- index.php
+++ index.php
@@ -102,6 +102,9 @@ $locations = [
 
 	"patreon" => "https://patreon.com/catalyst",
 
+	"stream" => "https://www.youtube.com/channel/UCI8shN2z06O5cBJIfR2DIaA/live",
+	"yt" => "https://www.youtube.com/channel/UCI8shN2z06O5cBJIfR2DIaA/live",
+
 	// patron supporters!
 	"keeri.patreon" => "https://beta.catalystapp.co/Character/View/w5nms1u/",
 
